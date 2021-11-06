69a1cb6a28b70d70ad0523fb0225d515eed45373
Jarek Majka <vessekheal@gmail.com>
2021-11-06 18:08:20 +0100

    settings_local created

--- project/settings.py
+++ project/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from project.settings_local import *
+except ImportError:
+    pass
