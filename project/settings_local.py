66d2d152df432213f934e90e499d0ca350575780
Jarek Majka <vessekheal@gmail.com>
2021-11-06 22:10:31 +0100

    gitignore updated, lettings_local updated

--- project/settings_local.py
+++ project/settings_local.py
@@ -1,3 +1,12 @@
 DEBUG = True
 
-DATABASES = {}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'da3',
+       'USER': 'jarek',
+       'PASSWORD': 'password',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   },
+}
