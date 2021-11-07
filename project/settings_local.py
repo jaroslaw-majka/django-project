c09b4fe77087862a095e0f340ffe9649413be247
Jarek Majka <vessekheal@gmail.com>
2021-11-07 21:57:59 +0100

    db changed

--- project/settings_local.py
+++ project/settings_local.py
@@ -3,7 +3,7 @@ DEBUG = True
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'da3',
+       'NAME': 'db4',
        'USER': 'jarek',
        'PASSWORD': 'password',
        'HOST': 'localhost',
