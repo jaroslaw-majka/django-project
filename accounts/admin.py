4c3c4ee063c0486cc7f21be0cfd8884e00c13fc1
Jarek Majka <vessekheal@gmail.com>
2021-11-07 21:42:59 +0100

    admin.py updated, User registered

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import ExtendedUser
+
+
+@admin.register(ExtendedUser)
+class ExtendedUserAdmin(admin.ModelAdmin):
+    pass
