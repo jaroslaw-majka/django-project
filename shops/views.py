bd6b547a71b56f10d3df10aaa5917d2ea3f68bc2
Jarek Majka <vessekheal@gmail.com>
2021-11-20 11:53:59 +0100

    Invoice view created

--- shops/views.py
+++ shops/views.py
@@ -1,4 +1,11 @@
 # Django
 from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic import ListView
+
+# Project
+from shops.models import Invoice
+
+
+class InvoiceListView(ListView):
+    model = Invoice
